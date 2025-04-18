47db52c67815e45602dea3b5480cca961ec4af01
siri-2118 <spandana2118@gmail.com>
2025-04-18 14:45:40 +0530

    made some changes

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Movies=("RRR" "DJTILLU" "MURALI")
+
+echo "First movie is: ${MOVIES[0]}"
+echo "second movie is: ${MOVIES[1]}"
+echo "third movie is: ${MoVIES[2]}"
+echo "all movies: ${MOVIES[@]}
