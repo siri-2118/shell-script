1bb320ac1c56a56df4a638a64184eae2331c1488
siri-2118 <spandana2118@gmail.com>
2025-04-18 14:17:12 +0530

    made some changes

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How aree you?"
+echo "Suresh:: Hi Ramesh, i am fine. How are you?"
+echo "Ramesh:: I am fine too, how was your work?"
+echo "Suresh:: It was good, but thinking to learn upgrade DevOps"
