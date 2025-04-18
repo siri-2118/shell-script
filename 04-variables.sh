d92c3727b18cf86b7bfeffe4fc928f0fdd578c71
siri-2118 <spandana2118@gmail.com>
2025-04-18 14:31:49 +0530

    made some changes

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2, How aree you?"
+echo "$PERSON2:: Hi $PERSON1, i am fine. How are you?"
+echo "$PERSON1:: I am fine too, how was your work?"
+echo "$PERSON2:: It was good, but thinking to learn upgrade DevOps"
